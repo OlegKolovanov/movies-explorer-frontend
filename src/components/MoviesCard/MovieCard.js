e259fe3f4c5cd6f5cd44e7a6ab2dfe837e2745f5
Oleg Kolovanov <olegkolovanov@yandex.ru>
2022-06-22 23:25:48 +0300

    5

--- src/components/MoviesCard/MovieCard.js
+++ src/components/MoviesCard/MovieCard.js
@@ -1,16 +1,25 @@
+import React, { useState } from 'react';
 import './MovieCard.css'
 import pick1 from '../../images/firstpicture.jpeg'
 import { useLocation } from 'react-router-dom';
 
 function MovieCard() {
     const location = useLocation()
+    const [isLiked, setIsLiked] = useState(false);
+    function handleLikeCard() {
+        setIsLiked(true)
+    }
+
+    const cardLikeButtonClassName = `${isLiked ? "moviecard__like_active" : "moviecard__like"
+        }`;
+
     return (
         <>
             <div className='moviecard'>
                 <img src={pick1} className='moviecard__image' alt='постер фильма'></img>
                 <div className='moviecard__container'>
                     <h2 className='moviecard__title'>33 слова о дизайне</h2>
-                    <button type='button' className={location.pathname === '/movies' ? 'moviecard__like' : 'moviecard__delete'}></button>
+                    <button type='button' onClick={handleLikeCard} className={location.pathname === '/movies' ? `${cardLikeButtonClassName}` : 'moviecard__delete'}></button>
                 </div>
                 <p className='moviecard__time'>1ч 42м</p>
             </div>
--- src/components/SerchForm/SearchForm.js
+++ src/components/SerchForm/SearchForm.js
@@ -3,7 +3,7 @@ import './SearchForm.css'
 function SearchForm() {
     return (
         <form className='search'>
-            <input className='search__input' placeholder='Фильм'></input>
+            <input className='search__input' placeholder='Фильм' required></input>
             <button type='button' className='search__button'>Поиск</button>
             <button type='button' className='search__checkbox'>Короткометражки</button>
         </form>
